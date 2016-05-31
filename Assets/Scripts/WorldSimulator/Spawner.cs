64bb6e45540faee11d4cebd164aef0b722590622
nb1949 <noam00bloom@gmail.com>
2016-05-31 13:05:35 +0300

    Missing Files

---
+++ Assets/Scripts/WorldSimulator/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public int spawnInvokeRate;
+	public int minSpawnRate;
+	public int maxSpawnRate;
+	public float spawnOffset;
+	public float avoidOffset;
+	public LayerMask avoid;
+	public Transform parent;
+	public GameObject spawnedObject;
+	public CreaturesStatistics creaturesStatistics;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("InvokeNextSpawn", spawnInvokeRate, spawnInvokeRate);
+	}
+
+	void InvokeNextSpawn() {
+		Invoke ("Spawn", Random.Range (minSpawnRate, maxSpawnRate));
+	}
+
+	public void Spawn() {
+		Vector2 spawnPosition = creaturesStatistics.meanPosition + 
+			Random.insideUnitCircle.normalized * Camera.main.orthographicSize * 2
+			+ Random.insideUnitCircle * spawnOffset;
+		if (!Physics2D.OverlapCircle (spawnPosition, avoidOffset, avoid)) {
+			GameObject spawned = (GameObject)GameObject.Instantiate (spawnedObject, spawnPosition, Quaternion.identity);
+			spawned.transform.SetParent (parent);
+		}
+	}
+
+
+}
