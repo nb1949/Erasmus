4e567e32c62ce96429e4212d47583f00ec29a238
nb1949 <noam00bloom@gmail.com>
2016-06-02 01:27:55 +0300

    Added Restart

--- Assets/Scripts/UI/OnClickPlay.cs
+++ Assets/Scripts/UI/OnClickPlay.cs
@@ -6,14 +6,17 @@ public class OnClickPlay : MonoBehaviour {
 
 	public Image image;
 	public Sprite nextSprite;
+	public GameObject restart;
 	private bool firstTime = true;
 	public GameObject game;
 	public GameObject gameUI;
 	public GameObject menuUI;
 
 	public void PlayGame() {
-		if (firstTime) 
+		if (firstTime) {
 			image.overrideSprite = nextSprite;
+			restart.SetActive (true);
+		}
 		game.SetActive (true);
 		gameUI.SetActive (true);
 		menuUI.SetActive (false);
---
+++ Assets/Scripts/UI/OnClickRestart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OnClickRestart : MonoBehaviour {
+
+	public void RestartGame() {
+		SceneManager.LoadScene ("main");
+	}
+}
