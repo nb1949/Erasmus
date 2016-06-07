b5dae08106ad551521203865d78a5ab779d17aed
nb1949 <noam00bloom@gmail.com>
2016-06-07 16:03:58 +0300

    Add depth by y feature to graphics

---
+++ Assets/Scripts/Utils/DepthSortByY.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Renderer))]
+public class DepthSortByY : MonoBehaviour
+{
+
+	private const int IsometricRangePerYUnit = 40;
+
+	void Update()
+	{
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.sortingOrder = -(int)(transform.position.y * IsometricRangePerYUnit);
+	}
+}
