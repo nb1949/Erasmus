b7310eafba13ab05c65e47b1aebbe78e36880f88
nb1949 <noam00bloom@gmail.com>
2016-07-10 01:19:18 +0300

    Added Cheats E-DNA boost W-Wind Visualization

--- Assets/Scripts/WorldSimulator/Winds/WindTile.cs
+++ Assets/Scripts/WorldSimulator/Winds/WindTile.cs
@@ -5,9 +5,31 @@ using UnityEngine.SocialPlatforms;
 public class WindTile : MonoBehaviour {
 
 	public AreaEffector2D af;
+	public GameObject graphics;
 	public float tempMagnitude, tempAngle;
 
 	void Awake () {
-		af = GetComponent <AreaEffector2D> ();
+		if(af == null)
+			af = GetComponent <AreaEffector2D> ();
+		if (graphics != null && graphics.activeInHierarchy)
+			InvokeRepeating ("UpdateWindGraphics", 1, 1);
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.W) && graphics != null){
+			if(!graphics.activeInHierarchy)  {
+				graphics.SetActive (true);
+				InvokeRepeating ("UpdateWindGraphics", 1, 1);
+			} else {
+				graphics.SetActive (false);
+				CancelInvoke ("UpdateWindGraphics");
+			}
+		}
+	}
+
+	private void UpdateWindGraphics() {
+			graphics.transform.localRotation = Quaternion.AngleAxis(af.forceAngle, Vector3.forward);
+			graphics.GetComponent<SpriteRenderer> ().color = 
+				new Color(255,255,255, 0.25f + af.forceMagnitude * 0.15f);
 	}
 }
